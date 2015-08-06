38112a1ca66ca26432ef366d5cc7d52d07059466
Maya Nedeljkovich <maja.nedeljkovic@sbgenomics.com>
2015-08-06 17:01:23 +0200

    Input fields and dropdowns.

--- src/app/components/ui/button/button.js
+++ src/app/components/ui/button/button.js
@@ -2,17 +2,15 @@
  * Created by Maya on 4.8.15.
  */
 
-import ControlElement from '../control-element/controlElement.directive.js';
+import ControlElement from '../control-element/controlElement.directive';
 
 class ButtonDirective extends ControlElement {
 	constructor(template) {
 		super();
 
-		this.transclude = true;
 		this.templateUrl = template || 'app/components/ui/button/button.html';
 		this.controller = ButtonController;
 		this.controllerAs = 'btn';
-
 		this.scope = {
 			intention: '@',
 			size: '@'
@@ -28,8 +26,10 @@ class LinkDirective extends ButtonDirective {
 
 class ButtonController {
 	constructor() {
-		this.intention = this.intention ? 'btn-' + this.intention : null;
-		this.size = this.size ? 'btn-' + this.size : null;
+	}
+
+	prependBtn(attr) {
+		return !_.isEmpty(attr) ? 'btn-' + attr : '';
 	}
 }
 
--- src/app/components/ui/input/input.js
+++ src/app/components/ui/input/input.js
@@ -2,28 +2,41 @@
  * Created by Maya on 4.8.15.
  */
 
-import ControlElement from '../control-element/controlElement.directive.js';
+import ControlElement from '../control-element/controlElement.directive';
 
 class InputDirective extends ControlElement {
 	constructor(type) {
 		super();
+
 		this.templateUrl = 'app/components/ui/input/' + (type || 'input') + '.html';
 		this.controller = InputController;
 		this.controllerAs = 'input';
 		this.scope = {
 			model: '=ngModel',
-			id: '=?'
+			id: '@'
 		};
+
+		this.link = function (scope, element, attr) {
+			element.find('input').bind('focus', function() {
+				element.find('.ct-input').addClass('ct-focus')
+			});
+
+			element.find('input').bind('blur', function() {
+				element.find('.ct-input').removeClass('ct-focus');
+			});
+		}
 	}
 }
 
 class InputController {
 	constructor ($log) {
-		this.id = this.id || _.uniqueId('input-');
+		this.$log = $log;
+		this.id = this.id ||  _.uniqueId('input-');
 	}
-
-
 }
 
+InputController.$inject = ['$log'];
+
 angular.module('cottontail').directive('ctSearch', () => new InputDirective('search'));
+angular.module('cottontail').directive('ctEmail', () => new InputDirective('email'));
 angular.module('cottontail').directive('ctInput', () => new InputDirective());
