ca2d226d11071da6ff496e4665325bf39b02ed04
Maja Nedeljković <maja.nedeljkovic@sbgenomics.com>
2016-02-16 19:09:41 +0100

    style(Cottontail): reformat EditorController

--- src/app/components/editor/editor.controller.js
+++ src/app/components/editor/editor.controller.js
@@ -6,26 +6,25 @@ import * as config from '../../constants/editor.const';
 import * as key from '../../services/Shortcuts';
 const _private = new WeakMap();
 const _editor = {};
-const _callbacks = {};
-let shortcuts = [];
 
 class Editor {
-	constructor() {
-		this.config = {
-			theme: config.EDITOR_THEME,
-			onLoad: load
-		};
-	}
+    constructor() {
+        this.config = {
+            theme: config.EDITOR_THEME,
+            mode: 'json',
+            onLoad: load
+        };
+    }
 
-	setMode (mode) {
-		this.config.mode = mode;
-	}
+    setMode(mode) {
+        this.config.mode = mode;
+    }
 }
 
-const load = function (editor) {
-	_private.set(_editor, editor);
+const load = function(editor) {
+    _private.set(_editor, editor);
 
-	editor.$blockScrolling = Infinity;
+    editor.$blockScrolling = Infinity;
 };
 
 angular.module('cottontail').service('Editor', Editor);
