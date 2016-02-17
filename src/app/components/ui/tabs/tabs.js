d2e3f57c632accc536db7cd53f8bcf9231c3c9e0
Maja Nedeljković <maja.nedeljkovic@sbgenomics.com>
2016-02-17 11:57:04 +0100

    style(Cottontail): reformat TabsDirective

--- src/app/components/ui/tabs/tabs.js
+++ src/app/components/ui/tabs/tabs.js
@@ -1,36 +1,36 @@
 import BaseElement from "../control-element/controlElement.directive";
 
 class TabsDirective extends BaseElement {
-	constructor() {
-		super();
+    constructor() {
+        super();
 
-		this.controller = TabsController;
-		this.controllerAs = 'tabs';
-		this.templateUrl = 'app/components/ui/tabs/tabs.html';
-		this.transclude = true;
-		this.replace = true;
-		this.scope = {
-			list: '=tabs',
-			onActiveTab: '&',
-			tabSrc: '@',
-			currentActiveTab: '=',
-			onCloseTab: '&'
-		};
+        this.controller = TabsController;
+        this.controllerAs = 'tabs';
+        this.templateUrl = 'app/components/ui/tabs/tabs.html';
+        this.transclude = true;
+        this.replace = true;
+        this.scope = {
+            list: '=tabs',
+            onActiveTab: '&',
+            tabSrc: '@',
+            currentActiveTab: '=',
+            onCloseTab: '&'
+        };
 
-        this.link = function (scope, elem, attr) {
-	        scope.tabs.attr = attr;
+        this.link = function(scope, elem, attr) {
+            scope.tabs.attr = attr;
             scope.tabs.list = _.isArray(scope.tabs.list) ? scope.tabs.list : [scope.tabs.list];
 
             if (_.isString(scope.tabs.list[0])) {
-            	scope.tabs.list = _.map(scope.tabs.list, function(tab) {
-            		return {
-            			name: tab,
-            			slug: _.kebabCase(tab)
-            		};
-            	});
+                scope.tabs.list = _.map(scope.tabs.list, function(tab) {
+                    return {
+                        name: tab,
+                        slug: _.kebabCase(tab)
+                    };
+                });
             }
 
-            scope.$watch('tabs.currentActiveTab', function (n, o) {
+            scope.$watch('tabs.currentActiveTab', function(n, o) {
                 if (n !== o) {
                     _.forEach(scope.tabs.list, function(tab) {
                         if (tab === n) {
@@ -41,13 +41,14 @@ class TabsDirective extends BaseElement {
             });
 
         };
-	}
+    }
 }
 
 class TabsController {
-	constructor () {}
+    constructor() {
+    }
 
-    activateTab (tab) {
+    activateTab(tab) {
         if (tab.slug === this.activeTab) {
             return;
         }
@@ -59,9 +60,9 @@ class TabsController {
         }
     }
 
-	closeTab (tab) {
-		this.onCloseTab({tab: tab});
-	}
+    closeTab(tab) {
+        this.onCloseTab({tab: tab});
+    }
 }
 
 angular.module('cottontail').directive('ctTabs', () => new TabsDirective());
