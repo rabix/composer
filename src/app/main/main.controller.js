a360c4fa9a96d3918201c7877eb3ceca26b5c5c3
Mate Balo Bordas <mate.balobordas@sbgenomics.com>
2016-02-22 19:26:28 +0100

    Fix(cottontail): fixed misspelled property in main.controller, and added bower install as a "post install" step for npm install

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,9 +1,9 @@
 class MainController {
     constructor(Api) {
         'ngInject';
-        Api.file.query().$promise.then(function(result){
+      /*  Api.files.query().$promise.then(function(result){
             console.log(result);
-        });
+        });*/
 
     }
 }
