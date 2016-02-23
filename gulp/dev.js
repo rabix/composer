3a4b45157f2be864a810b38020facbbac9c184cd
Filip Jelic <jelic.filip@gmail.com>
2016-02-24 00:35:38 +0100

    added dev task

---
+++ gulp/dev.js
@@ -0,0 +1,12 @@
+/**
+ * Created by filip on 2/24/16.
+ */
+"use strict";
+
+var path = require('path');
+var gulp = require('gulp');
+var conf = require('./conf');
+
+var $ = require('gulp-load-plugins')();
+
+gulp.task('dev', ['inject'], function () {});
