866a1dcf0b1dbd2482df7f824ff59eeface3afb1
Maja Nedeljković <maja.nedeljkovic@sbgenomics.com>
2016-05-13 14:30:34 +0200

    Added socket requests that match backend.

--- src/app/services/api/socket-events.ts
+++ src/app/services/api/socket-events.ts
@@ -1,7 +1,9 @@
 export const SOCKET_REQUESTS = {
-    "DIR_CONTENT": "getDirContents"
+    "DIR_CONTENT": "getDirContents",
+    "FILES_IN_WORKSPACE": "getFilesInWorkspace",
+    "FILE_CONTENT": "getFile",
+    "CREATE_FILE": "createFile",
+    "UPDATE_FILE": "updateFile"
 };
 
-export const SOCKET_EVENTS = {
-    
-};
+export const SOCKET_EVENTS = [];
