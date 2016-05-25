f896bcbc7448d89710d15d21135dfefef61824d7
Maja Nedeljković <maja.nedeljkovic@sbgenomics.com>
2016-05-25 18:06:59 +0200

    Added logic for loading file content

--- src/app/components/code-editor/code-editor.component.ts
+++ src/app/components/code-editor/code-editor.component.ts
@@ -2,35 +2,51 @@ import {Component, OnInit, ElementRef} from "@angular/core";
 import {CodeEditor} from "./code-editor.service";
 import Editor = AceAjax.Editor;
 import TextMode = AceAjax.TextMode;
+import {FileRegistry, File} from "../../services/file-registry.service";
 
 require('./code-editor.component.scss');
-
-require('brace/theme/monokai');
-require('brace/mode/javascript');
 
 @Component({
     selector: 'code-editor',
     inputs: ['text'],
     template: `<div class="code-editor-container">
                     <div class="editor"></div>
-               </div>`
+               </div>`,
+    providers: [FileRegistry]
 })
 export class CodeEditorComponent implements OnInit {
     editor: CodeEditor;
+    file: File;
 
-    constructor(private elem: ElementRef) {
+    constructor(private elem: ElementRef, private fileRegistry: FileRegistry) {
     }
 
     ngOnInit(): any {
         let editorInstance = ace.edit(this.elem.nativeElement.getElementsByClassName('editor')[0]);
         this.editor        = new CodeEditor(editorInstance);
+
+        // this check shouldn't be necessary
+        if (this.file) {
+            this.editor.setMode(this.file.type);
+            this.file.content = this.fileRegistry
+                .fetchFileContent(this.file);
+            
+            this.file.content.subscribe(text => {
+               if (text !== null) {
+                  this.editor.setText(text);
+               } 
+            });
+        }
     }
 
     ngOnDestroy(): any {
         this.editor.dispose();
     }
 
-    public setState(state){
-        console.log("Setting", state);
+    public setState(state) {
+        // @todo figure out why this is undefined on startup
+        if (state.fileInfo) {
+            this.file = new File(state.fileInfo);
+        }
     }
 }
