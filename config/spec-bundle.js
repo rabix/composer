202f4f22941ede5f0b193df8bf38d210af254506
Ivan Batić <ivan.batic@live.com>
2016-06-16 15:23:56 +0200

    Updated import requirements.

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -9,7 +9,9 @@
  */
 Error.stackTraceLimit = Infinity;
 
-require('core-js');
+require('core-js/es6');
+require('core-js/es7/reflect');
+
 
 // Typescript emit helpers polyfill
 require('ts-helpers');
@@ -18,6 +20,8 @@ require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
 require('zone.js/dist/jasmine-patch');
 require('zone.js/dist/async-test');
+require('zone.js/dist/fake-async-test');
+require('zone.js/dist/sync-test');
 
 // RxJS
 require('rxjs/Rx');
