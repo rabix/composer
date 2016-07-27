3a9b77c2109fb318ff7753ac0cb2b8ec9e10b7cd
Mate Balo Bordas <mate.balob@gmail.com>
2016-07-27 15:46:53 +0200

    separate tool-container HTML

--- src/app/components/tool-container/tool-container.component.ts
+++ src/app/components/tool-container/tool-container.component.ts
@@ -5,10 +5,10 @@ import {FileModel} from "../../store/models/fs.models";
 import {CodeEditorComponent} from "../code-editor/code-editor.component";
 import {GuiEditorComponent} from "../gui-editor/gui-editor.component";
 
-require('./tool-container.component.scss');
+require("./tool-container.component.scss");
 
 @Component({
-    selector: 'editor-wrapper',
+    selector: "tool-container",
     directives: [
         CodeEditorComponent,
         GuiEditorComponent,
@@ -18,43 +18,14 @@ require('./tool-container.component.scss');
         NgSwitchDefault,
         NgSelectOption
     ],
-    template: `
-                <div id="viewContainer">
-                    <header>
-                        <select id="revisionSelect" name="rev" [ngModel]="selectedRevision" (change)="onChange($event)">
-                            <option *ngFor="let revision of revisions" [value]="revision">{{revision}}</option>
-                        </select>
-                    
-                        <span id="guiJsonButtons">
-                            <button type="button" 
-                                    class="btn btn-secondary btn-sm selected" 
-                                    [ngClass]="{selected: viewMode === 'json'}"
-                                    (click)="setViewMode('json')">JSON</button>
-                                    
-                            <button type="button"
-                                    class="btn btn-secondary btn-sm" 
-                                    [ngClass]="{selected: viewMode === 'gui'}"
-                                    (click)="setViewMode('gui')">GUI</button>
-                        </span>
-                        
-                        <button id="saveButton" type="button" class="btn btn-secondary btn-sm">Save</button>
-                    </header>
-                   
-                    <div [ngSwitch]="viewMode">
-                        <block-loader *ngIf="!file"></block-loader>
-                        
-                        <gui-editor *ngSwitchDefault [file]="file"></gui-editor>
-                        <gui-editor *ngSwitchCase="'gui'" [file]="file"></gui-editor>
-                        <code-editor *ngSwitchCase="'json'" [file]="file"></code-editor>
-                    </div>
-                </div>`,
+    template: require("./tool-container.html")
 })
 export class ToolContainerComponent implements OnInit {
-    viewMode: string = 'gui';
+    viewMode: string = "gui";
     file: FileModel;
 
     /* TODO: load actual revisions */
-    revisions: Array<string> = ['rev1', 'rev2', 'rev3'];
+    revisions: Array<string> = ["rev1", "rev2", "rev3"];
     selectedRevision: string = this.revisions[0];
 
     constructor() {}
