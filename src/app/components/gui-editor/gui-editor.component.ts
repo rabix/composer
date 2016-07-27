c14e035b4d9e9bbc813a69b0d1ae7c3b91fbd169
Mate Balo Bordas <mate.balob@gmail.com>
2016-07-27 15:47:33 +0200

    add commandline to the gui-editor

--- src/app/components/gui-editor/gui-editor.component.ts
+++ src/app/components/gui-editor/gui-editor.component.ts
@@ -1,14 +1,41 @@
-import {Component, OnInit, Input} from "@angular/core";
+import {
+    Component,
+    OnInit,
+    Input,
+    trigger,
+    state,
+    style,
+    transition,
+    animate
+} from "@angular/core";
 import {NgFor} from "@angular/common";
 import {FileRegistry} from "../../services/file-registry.service";
 import {FileModel} from "../../store/models/fs.models";
 import {PropertyInput} from "../forms/inputs/property-input.component";
 
-require('./gui-editor.component.scss');
+require("./gui-editor.component.scss");
+
+type CommandLineState = "visible" | "hidden";
 
 @Component({
-    selector: 'gui-editor',
+    selector: "gui-editor",
     directives: [NgFor, PropertyInput],
+    animations: [
+        trigger("commandlineState", [
+            state("visible", style({
+                height: 70,
+                display: "block",
+                overflowY: 'auto'
+            })),
+            state("hidden",   style({
+                height: 20,
+                display: "none",
+                overflowY: 'hidden'
+            })),
+            transition("hidden => visible", animate("100ms ease-in")),
+            transition("visible => hidden", animate("100ms ease-out"))
+        ])
+    ],
     template: `
                 <div id="guiEditorContainer">
                     <property-input *ngFor="let property of mockInputProperties"
@@ -18,14 +45,27 @@ require('./gui-editor.component.scss');
                     </property-input>
                     
                     <footer>
-                        <button type="button" class="btn btn-secondary btn-sm">Issues</button>
-                        <button type="button" class="btn btn-secondary btn-sm">Resulting Command</button>
+                        <div id="commandline"
+                             class="commandlineBar"
+                             @commandlineState="commandlineState">
+                            {{commandlineContent}}
+                        </div>
+                    
+                        <div class="footerButtons">
+                            <button type="button" class="btn btn-secondary btn-sm">Issues</button>
+                            <button type="button" 
+                            class="btn btn-secondary btn-sm"
+                            (click)="toggleCommandLine()">Resulting Command</button>
+                        </div>
                     </footer>
                 </div>
     `
 })
 export class GuiEditorComponent implements OnInit {
     @Input() file: FileModel;
+    commandlineState: CommandLineState = 'hidden';
+    /*TODO: generate the commandline*/
+    commandlineContent: string = 'This is the command line';
 
     /* TODO: get tool properties for display, create a service that returns a list of properties based on the tool */
     /*mockInputProperties: Array<any> = [
@@ -55,6 +95,10 @@ export class GuiEditorComponent implements OnInit {
 
     ngOnInit(): void {
 
+    }
+
+    toggleCommandLine() {
+        this.commandlineState = this.commandlineState === 'hidden' ? 'visible': 'hidden';
     }
 
 }
