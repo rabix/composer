27416179d9944d94dd3508531d8a0212ec45ac06
Ivan Batic <ivan@Ivans-MacBook-Pro-2.local>
2016-07-29 18:22:48 +0200

    Replaced autogenerated docs with our doc folder.

--- src/app/decorators/index.ts
+++ src/app/decorators/index.ts
@@ -1,6 +1,11 @@
 export const DECORATOR_KEY_ASSIGNABLE = "ct:assignable";
 
-export const assignable = (method ?: string) => (target: Object, key: string) => {
+/**
+ * Property decorator that makes a class property assignable by using the Chap.Component.assign function.
+ *
+ * @param method Method to invoke on the target[key] property in order to set the assignable property
+ */
+export const assignable = (method?: string) => (target: Object, key: string) => {
     const metakey  = DECORATOR_KEY_ASSIGNABLE;
     const metadata = Reflect.getMetadata(metakey, target) || {};
     const update   = Object.assign(metadata, {[key]: {method}});
