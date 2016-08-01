2e8d96192cc7a84271ce0a398497c2470efcb172
Mate Balo Bordas <mate.balob@gmail.com>
2016-08-01 11:44:46 +0200

    remove revision select from tool-header

--- src/app/components/tool-container/tool-header/tool-header.component.ts
+++ src/app/components/tool-container/tool-header/tool-header.component.ts
@@ -14,20 +14,17 @@ require("./tool-header.component.scss");
     directives: [NgSelectOption],
     template: `
         <header>
-            <select id="revisionSelect" name="rev" [ngModel]="selectedRevision" (change)="onRevisionChange($event)">
-                <option *ngFor="let revision of revisions" [value]="revision">{{revision}}</option>
-            </select>
-                            <span id="guiJsonButtons">
-                                <button type="button"
-                                        class="btn btn-secondary btn-sm selected"
-                                        [ngClass]="{selected: viewMode === 'json'}"
-                                        (click)="changeViewMode('json')">JSON</button>
-    
-                                <button type="button"
-                                        class="btn btn-secondary btn-sm"
-                                        [ngClass]="{selected: viewMode === 'gui'}"
-                                        (click)="changeViewMode('gui')">GUI</button>
-                            </span>
+                <span id="guiJsonButtons">
+                    <button type="button"
+                            class="btn btn-secondary btn-sm selected"
+                            [ngClass]="{selected: viewMode === 'json'}"
+                            (click)="changeViewMode('json')">JSON</button>
+
+                    <button type="button"
+                            class="btn btn-secondary btn-sm"
+                            [ngClass]="{selected: viewMode === 'gui'}"
+                            (click)="changeViewMode('gui')">GUI</button>
+                </span>
     
             <button id="saveButton" type="button" class="btn btn-secondary btn-sm">Save</button>
         </header>
@@ -35,24 +32,14 @@ require("./tool-header.component.scss");
 })
 export class ToolHeaderComponent {
     /** The current view mode is needed for styling the selected button */
-    @Input() 
+    @Input()
     private viewMode: ViewMode;
-    
+
     /** Emit changes of the view mode */
-    @Output() 
+    @Output()
     private viewModeChanged = new EventEmitter();
-    
-    /** Tool revisions. TODO: load actual revisions */
-    private revisions: Array<string> = ["rev1", "rev2", "rev3"];
-    
-    /** Currently selected revision */
-    private selectedRevision: string = this.revisions[0];
-    
+
     changeViewMode(viewMode: string) {
         this.viewModeChanged.emit(viewMode);
-    }
-
-    onRevisionChange(e): void {
-        this.selectedRevision = e.target.value;
     }
 }
