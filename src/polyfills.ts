29e0db5dba173d2434aee26bc3476002095e3739
Mate Balo Bordas <mate.balob@gmail.com>
2016-08-25 12:20:10 +0200

    polyfills need to require 'zone.js/dist/sync-test' since the new version of zone.js

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,6 +3,7 @@ import "core-js/es6";
 import "core-js/es7/reflect";
 import "ts-helpers";
 require('zone.js/dist/zone');
+require('zone.js/dist/sync-test');
 
 if ('production' === ENV) {
     // Production
