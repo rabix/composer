d5bd48ae245a191836630cfcd0d982d39fb8ff72
ivanbatic <ivan.batic@live.com>
2016-10-20 19:17:04 +0200

    Fixed the build.

--- scripts/electron-build.sh
+++ scripts/electron-build.sh
@@ -3,8 +3,9 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 
 cd $BASEDIR
 
-cp ./electron/package.prod.json ./dist/package.json
+cp ./electron/package.json ./dist/package.json
 cp ./electron/main.prod.js ./dist/main.js
-cp -r ./electron/src ./dist/
+cp -r ./electron/src ./dist
+cp -r ./electron/node_modules ./dist
 
 ./node_modules/.bin/electron-packager ./dist "rabix-editor-$(date +%s)" --overwrite --out build --icon ./electron/rabix-icon.icns --build-version "0.1.0"
