b4fb80d72246a300134433b9de3298f054db622b
Ivan Batic <ivan.batic@live.com>
2016-12-26 20:45:14 +0100

    Fixed the build by turning off source maps.

--- config/webpack.electron.js
+++ config/webpack.electron.js
@@ -43,7 +43,7 @@ module.exports = webpackMerge(commonConfig, {
      * See: http://webpack.github.io/docs/configuration.html#devtool
      * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
      */
-    devtool: 'source-map',
+    devtool: '',
 
     entry: {
 
