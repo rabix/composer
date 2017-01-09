b79c5017cb73b62d98925a69ad85815f8f087c97
Ivan Batic <ivan.batic@live.com>
2017-01-09 14:49:03 +0100

    Updated code style.

--- src/app/platform-providers/user-projects/electron-user-projects.service.ts
+++ src/app/platform-providers/user-projects/electron-user-projects.service.ts
@@ -1,39 +1,40 @@
 import {Injectable} from "@angular/core";
-import {LocalDataSourceService} from "../../sources/local/local.source.service";
-import {UserProjectsService} from "./user-projects.service";
+import {noop} from "../../lib/utils.lib";
 import {MenuItem} from "../../core/ui/menu/menu-item";
+import {UserProjectsService} from "./user-projects.service";
+import {LocalDataSourceService} from "../../sources/local/local.source.service";
 
 const {app, dialog} = window.require("electron").remote;
 
 @Injectable()
 export class ElectronUserProjectsService extends UserProjectsService {
 
-    constructor (private fs: LocalDataSourceService) {
+    constructor(private fs: LocalDataSourceService) {
         super();
     }
 
     public getContextMenu(name, content): MenuItem[] {
-        const items = [];
 
-        const newFile = new MenuItem("Copy to Local Files...", {
-            click: () => {
-                dialog.showSaveDialog({
-                    title: "Choose a Name",
-                    defaultPath: [app.getPath("home"), name + ".json"].join("/"),
-                    buttonLabel: "Copy"
-                }, (path) => {
-                    if (path) {
-                        content.first().switchMap(cont => this.fs.createFile(path, cont).catch(() => {
-                            return this.fs.getContentSavingFunction(path)(cont);
-                        })).subscribe(() => {},
-                            error => dialog.showErrorBox("Error", "An unknown error has occurred!"));
-                    }
-                });
-            }
-        });
+        return [
+            new MenuItem("Copy to Local Files...", {
+                click: () => {
 
-        items.push(newFile);
+                    dialog.showSaveDialog({
+                        title: "Choose a Name",
+                        defaultPath: `${app.getPath("home")}/${name}.json`,
+                        buttonLabel: "Copy"
+                    }, path => {
+                        if (!path) {
+                            return;
+                        }
 
-        return items;
+                        content.first()
+                            .switchMap(cont => this.fs.createFile(path, cont)
+                                .catch(() => this.fs.getContentSavingFunction(path)(cont)))
+                            .subscribe(noop, error => dialog.showErrorBox("Error!", error));
+                    });
+                }
+            })
+        ];
     }
 }
