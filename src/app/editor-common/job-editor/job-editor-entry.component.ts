7369992aa61bafa9610f745e902fc0268fe5aa09
Ivan Batic <ivan.batic@live.com>
2017-01-25 12:14:36 +0100

    Job Editor - Made the addition and deletion of new array elements work.

--- src/app/editor-common/job-editor/job-editor-entry.component.ts
+++ src/app/editor-common/job-editor/job-editor-entry.component.ts
@@ -5,66 +5,77 @@ import {
     Output,
     EventEmitter,
     SimpleChanges,
-    OnChanges,
-    ChangeDetectorRef
+    OnChanges
 } from "@angular/core";
 import {CommandInputParameterModel} from "cwlts/models/d2sb";
+import {JobHelper} from "cwlts/models/helpers/JobHelper";
 
 @Component({
     selector: "ct-job-editor-entry",
     changeDetection: ChangeDetectionStrategy.OnPush,
     template: `
         <div [ngSwitch]="input.type.type" class="form-group">
-            <!--Enums-->
-            <template ngSwitchCase="enum">
-                <select [value]="value" class="form-control" [attr.inputId]="input.id" [attr.arrayIndex]="index">
-                    <option *ngFor="let val of input.type.symbols" [value]="val"> {{ val }}</option>
-                </select>
-            </template>
+        
+            <!--Each leaf field will be wrapped as an input group-->
+            <!--Nested fields below should not be wrapped into other container elements-->
+            <!--because it will break size and positioning-->
+            <div class="input-group">
             
-            <!--Numbers-->
-            <template ngSwitchCase="int">
-                <input [attr.inputId]="input.id" [attr.arrayIndex]="index" type="number" class="form-control" [value]="value"/>
-            </template>
-            <template ngSwitchCase="float">
-                <input [attr.inputId]="input.id" [attr.arrayIndex]="index" type="number" class="form-control" [value]="value"/>
-            </template>
-            
-            <!--Strings-->
-            <template ngSwitchCase="string">
-                <input [attr.inputId]="input.id" [attr.arrayIndex]="index" class="form-control" [value]="value"/>
-            </template>
-            
-            <!--Booleans-->
-            <template ngSwitchCase="boolean">
-                <ct-toggle-slider [attr.inputId]="input.id" [attr.arrayIndex]="index" class="pull-right" [value]="value"></ct-toggle-slider>
-            </template>
-            
-            <!--Files-->
-            <template ngSwitchCase="File">
-                <div class="clickable" >
-                    <div class="input-group">
-                        <input [attr.inputId]="input.id" [attr.arrayIndex]="index" [attr.jobPropPath]="'path'" class="form-control" [value]="value?.path"/>
-                        <span class="input-group-btn">
-                            <button type="button" class="btn btn-secondary" 
-                                    [ct-editor-inspector]="fileInspector">
-                                <i class="fa fa-ellipsis-h"></i>
-                            </button>
-                        </span>  
-                    </div>
-                </div>
-                
-                <template #fileInspector>
-                    <ct-editor-inspector-content>
-                        <div class="tc-header">{{ input.id }}</div>
-                        <div class="tc-body">
-                            <ct-file-input-inspector [input]="value || {}" 
-                                                     (update)="updateJob($event)">
-                            </ct-file-input-inspector>
-                        </div>
-                    </ct-editor-inspector-content>
+                <!--Enums-->
+                <template ngSwitchCase="enum">
+                    <select [value]="value" class="form-control" [attr.inputId]="input.id" [attr.arrayIndex]="index">
+                        <option *ngFor="let val of input.type.symbols" [value]="val"> {{ val }}</option>
+                    </select>
                 </template>
-            </template>
+                
+                <!--Numbers-->
+                <template ngSwitchCase="int">
+                    <input [attr.inputId]="input.id" [attr.arrayIndex]="index" type="number" class="form-control" [value]="value"/>
+                </template>
+                <template ngSwitchCase="float">
+                    <input [attr.inputId]="input.id" [attr.arrayIndex]="index" type="number" class="form-control" [value]="value"/>
+                </template>
+                
+                <!--Strings-->
+                <template ngSwitchCase="string">
+                    <input [attr.inputId]="input.id" [attr.arrayIndex]="index" class="form-control" [value]="value"/>
+                </template>
+                
+                <!--Booleans-->
+                <template ngSwitchCase="boolean">
+                    <ct-toggle-slider [attr.inputId]="input.id" [attr.arrayIndex]="index" class="pull-right" [value]="value"></ct-toggle-slider>
+                </template>
+                
+                <!--Files-->
+                <template ngSwitchCase="File">
+                    <input [attr.inputId]="input.id" [attr.arrayIndex]="index" [attr.jobPropPath]="'path'" class="form-control" [value]="value?.path"/>
+                    <span class="input-group-btn">
+                        <button type="button" class="btn btn-secondary" 
+                                [ct-editor-inspector]="fileInspector">
+                            <i class="fa fa-ellipsis-h"></i>
+                        </button>
+                    </span>  
+                    
+                    <template #fileInspector>
+                        <ct-editor-inspector-content>
+                            <div class="tc-header">{{ input.id }}</div>
+                            <div class="tc-body">
+                                <ct-file-input-inspector [input]="value || {}" 
+                                                         (update)="updateJob($event)">
+                                </ct-file-input-inspector>
+                            </div>
+                        </ct-editor-inspector-content>
+                    </template>
+                </template>
+                
+                <!--Every element that's a part of the array can be deleted, so we add a deletion button to it-->
+                <span class="input-group-btn" *ngIf="index !== -1">
+                    <button type="button" class="btn btn-secondary" (click)="deleteFromArray()">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                </span>  
+            
+            </div>
             
             <!--Records-->
             <!--Arrays-->
@@ -75,13 +86,12 @@ import {CommandInputParameterModel} from "cwlts/models/d2sb";
                                      (update)="updateArray(i, $event)"
                                      [value]="entry"></ct-job-editor-entry>
                                      
-                <button (click)="addArrayEntry({})" 
+                <button (click)="addArrayEntry(input)" 
                         type="button" 
                         class="btn pl-0 btn-link no-outline no-underline-hover">
                     <i class="fa fa-plus"></i> New {{ input.type.items }}
                 </button>
             </template>
-            
             
             <!--Unknown-->
             <template ngSwitchDefault>
@@ -112,6 +122,13 @@ export class JobEditorEntryComponent implements OnChanges {
 
     public updateArray(index, data) {
 
+        // We need some kind of convention to broadcast information
+        // that an array element should be deleted
+        if (data === undefined) {
+            this.updateJob(this.value.filter((e, i) => i !== index));
+            return;
+        }
+
         // This is tricky.
         // We need to update the original value in place, and cant replace its reference because
         // of the object inspector, which would still point to the previous entry.
@@ -121,8 +138,13 @@ export class JobEditorEntryComponent implements OnChanges {
         this.updateJob(this.value.slice());
     }
 
-    private addArrayEntry(data) {
-        this.updateJob(this.value.concat(data));
+    private addArrayEntry(input) {
+        const generatedEntry = JobHelper.getJobPart(input);
+        this.updateJob((this.value || []).concat(generatedEntry));
+    }
+
+    private deleteFromArray() {
+        this.updateJob(undefined);
     }
 
     ngOnChanges(changes: SimpleChanges) {
@@ -130,7 +152,10 @@ export class JobEditorEntryComponent implements OnChanges {
         if (this.input.type.type === "array") {
             this.arrayModifiedInput = {
                 ...this.input,
-                type: {...this.input.type, type: this.input.type.items}
+                type: {
+                    ...this.input.type,
+                    type: this.input.type.items
+                }
             }
         }
     }
--- src/app/editor-common/job-editor/job-editor.component.ts
+++ src/app/editor-common/job-editor/job-editor.component.ts
@@ -44,13 +44,13 @@ import {EditorInspectorService} from "../inspector/editor-inspector.service";
 export class JobEditorComponent implements OnChanges {
 
     /**
-     * Existing CWL job metadata
+     * Existing CWL job metadata.
      */
     @Input()
     public job: { allocatedResources?: { cpu: number, mem: number }, inputs?: {} } = {};
 
     /**
-     * CWL app input definitions
+     * CWL app input definitions.
      */
     @Input()
     public inputs: CommandInputParameterModel[] = [];
@@ -64,45 +64,82 @@ export class JobEditorComponent implements OnChanges {
     }
 
     /**
-     * Inputs grouped and sorted the way they should be presented
+     * Inputs grouped and sorted the way they should be presented.
      */
     public inputGroups: { name: string, inputs: CommandInputParameterModel[] }[] = [];
 
+    /**
+     * Executes when job values get edited in the top-level forms.
+     * We need to take those values and emit the new job structure as an update.
+     */
     private onJobFormChange(event: Event) {
-        console.log("Job form change");
 
+        const formField = event.target as Element;
+
+        // Make a copy of the current job so we can operate on it without modifying the shown data.
         const job = {...this.job};
 
-        const jobPropPath = event.target.getAttribute("jobPropPath");
-        const inputId     = event.target.getAttribute("inputID");
-        const arrayIndex  = event.target.getAttribute("arrayIndex");
-        const val         = event.target.value;
+        // ID of the input that the modification refers to.
+        const inputId = formField.getAttribute("inputID");
+
+        // Optional key of the optional nested property of the updated input's job value,
+        // ex. "path" in "job.inputs.de_results.path".
+        const jobPropPath = formField.getAttribute("jobPropPath");
+
+        // If the input is an array, we should find the index of the element here.
+        const arrayIndex = formField.getAttribute("arrayIndex");
+
+        // Get the new value that we should set the job to
+        const val = formField.value;
+
+        // Form field might not have input id, so if we missed it,
+        // it's better to do nothing than break the app.
         if (!inputId) {
             return;
         }
 
-        const isArr    = arrayIndex != -1;
+        // For ease of use, non-arrays will have -1 as an index value.
+        const isArr = arrayIndex != -1;
+
+        // Compose a path that looks like "inputs.de_results.[2].path",
+        // where the last 2 parts are optional.
         const propPath = [
             "inputs",
             inputId,
             !isArr ? "" : `[${arrayIndex}]`,
-            jobPropPath
-        ].filter(v => v).join(".");
+            jobPropPath].filter(v => v).join(".");
+
+        // Assign the new value to the previously made new path.
         OH.addProperty(job, propPath, val);
 
+        // Get the new value of this input's job.
+        // On the previous step, we might have set some nested property of it,
+        // so here we take the top-level structure anyway.
         let jobRef = OH.getProperty(job, `inputs.${inputId}`);
+
+        // Dispatch it as an update to the job
         this.jobValueUpdate(inputId, jobRef);
     }
 
+    /**
+     * Updates the job value for a given input and emits the updated job.
+     */
     private jobValueUpdate(inputId, jobValue) {
-        this.statusBar.instant(`Updated job value of ${inputId}.`);
 
-        const job           = {...this.job};
+        const input = this.inputs.find(i => i.id === inputId);
+
+        // Show a message in the status bar about what's changed.
+        this.statusBar.instant(`Updated job value of ${input.label || input.id}.`);
+
+        // Create a new reference of the job for the change detector
+        const job = {...this.job};
+
+        // Assign the given value to the job key
         job.inputs[inputId] = jobValue;
 
+        // Send the update to the output of this component
         this.update.emit(job);
         this.cdr.markForCheck();
-
     }
 
     ngOnChanges(changes: SimpleChanges) {
