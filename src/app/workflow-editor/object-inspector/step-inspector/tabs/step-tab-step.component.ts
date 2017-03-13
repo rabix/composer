f63aef624aa76fda2c088eee28c7153062837006
marijanlekic <marijan.lekic@sbgenomics.com>
2017-03-13 15:04:52 +0100

    Remove unnecessary div

--- src/app/workflow-editor/object-inspector/step-inspector/tabs/step-tab-step.component.ts
+++ src/app/workflow-editor/object-inspector/step-inspector/tabs/step-tab-step.component.ts
@@ -51,13 +51,11 @@ import {FormBuilder, FormGroup} from "@angular/forms";
                 </select>
             
                 <!--Multiple Scatter-->
-                <div *ngIf="step.hasMultipleScatter">
-                        <select class="form-control" multiple [formControl]="form.controls['scatter']">
-                            <option *ngFor="let opt of step.in" [value]="opt.id">
-                                {{opt.id}}
-                            </option>
-                        </select>
-                </div>
+                <select *ngIf="step.hasMultipleScatter" class="form-control" multiple [formControl]="form.controls['scatter']">
+                    <option *ngFor="let opt of step.in" [value]="opt.id">
+                        {{opt.id}}
+                    </option>
+                </select>
             
             </div>
             
