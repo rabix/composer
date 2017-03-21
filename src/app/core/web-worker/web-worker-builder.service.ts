0d299f14d3a97d3ff56a6e1d079b62dfb30548bb
Ivan Batic <ivan.batic@live.com>
2017-03-21 14:09:06 +0100

    Fixed an issue where worker compiler would have a question mark in the end of worker script origin

--- src/app/core/web-worker/web-worker-builder.service.ts
+++ src/app/core/web-worker/web-worker-builder.service.ts
@@ -6,17 +6,22 @@ export class WebWorkerBuilderService {
 
     create<T>(workerFunction: ((input: any) => T), scripts: string[] = [], context = {}): WebWorker<T> {
 
-        const url = this.compile(workerFunction, scripts, context);
+        const url    = this.compile(workerFunction, scripts, context);
         const worker = new Worker(url);
         return new WebWorker<T>(worker);
 
     }
 
     private compile(fn: Function, scripts = [], context = {}) {
-        let [origin]= document.location.href.replace("/index.html", "").split("#");
+        let [origin] = document.location.href.replace("/index.html", "").split("#");
+
         if (origin.endsWith("/")) {
             origin = origin.slice(0, -1);
         }
+        if (origin.endsWith("?")) {
+            origin = origin.slice(0, -1);
+        }
+        console.log("Compiled origin", origin, "from", document.location);
 
         const paths = scripts.map(s => s.startsWith("/") ? s.slice(1) : s)
             .map(s => "'" + [origin, s].join("/") + "'");
