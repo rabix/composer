c0014438f23b2f856b3a0bb13acef87de1132643
Ivan Batic <ivan.batic@live.com>
2017-03-29 18:24:19 +0200

    Added support for multiple handlers in the accelerator proxy

--- electron/src/accelerator-proxy.ts
+++ electron/src/accelerator-proxy.ts
@@ -1,14 +1,21 @@
-const registry = {};
+const registry: { [hash: string]: Function[] } = {};
 
 module.exports = {
 
     on: (accelerator, callback) => {
-        registry[accelerator] = callback;
+        if (!registry[accelerator]) {
+            registry[accelerator] = [];
+        }
+        registry[accelerator].push(callback);
     },
 
     pass: (menu, browser, event) => {
-        if (typeof registry[menu.accelerator] === "function") {
-            registry[menu.accelerator](menu, browser, event);
+        if (Array.isArray(registry[menu.accelerator])) {
+            registry[menu.accelerator].forEach(callback => {
+                if (typeof callback === "function") {
+                    callback(menu, browser, event);
+                }
+            });
         }
     }
 };
