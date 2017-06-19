ba06c1f73afeb287a0b7e404ce25bb2913170e18
Maja Nedeljković <maya.nedeljko@gmail.com>
2017-06-19 11:50:36 +0200

    chore(test): fixed test for directory-input-inspector

--- src/app/editor-common/inspector-forms/directory-input-inspector/directory-input-inspector.component.spec.ts
+++ src/app/editor-common/inspector-forms/directory-input-inspector/directory-input-inspector.component.spec.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 
 import {DirectoryInputInspectorComponent} from "./directory-input-inspector.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe("DirectoryInputInspectorComponent", () => {
     let component: DirectoryInputInspectorComponent;
@@ -8,6 +9,7 @@ describe("DirectoryInputInspectorComponent", () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [ReactiveFormsModule, FormsModule],
             declarations: [DirectoryInputInspectorComponent]
         })
             .compileComponents();
