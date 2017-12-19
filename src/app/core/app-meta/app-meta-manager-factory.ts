718122c922b65b148ece8e64ecf3adf88454a2e8
Ivan Batic <ivan.batic@live.com>
2017-12-19 11:41:08 +0100

    dep upgrade, auto-heal job to an object

--- src/app/core/app-meta/app-meta-manager-factory.ts
+++ src/app/core/app-meta/app-meta-manager-factory.ts
@@ -1,10 +1,10 @@
+import {InjectionToken} from "@angular/core";
 import {AppMetaEntry} from "../../../../electron/src/storage/types/app-meta";
 import {AppEditorBase} from "../../editor-common/app-editor-base/app-editor-base";
 import {LocalRepositoryService} from "../../repository/local-repository.service";
 import {PlatformRepositoryService} from "../../repository/platform-repository.service";
 import {AppHelper} from "../helpers/AppHelper";
 import {AppMetaManager} from "./app-meta-manager";
-import {InjectionToken} from "@angular/core";
 
 export function appMetaManagerFactory(editor: AppEditorBase,
                                       localRepository: LocalRepositoryService,
@@ -14,17 +14,25 @@ export function appMetaManagerFactory(editor: AppEditorBase,
 
     if (AppHelper.isLocal(appID)) {
         return {
-            getAppMeta: (key?: keyof AppMetaEntry) => localRepository.getAppMeta(appID, key),
-            patchAppMeta: (key: keyof AppMetaEntry, value: any) => localRepository.patchAppMeta(appID, key, value)
+            getAppMeta: (key?: keyof AppMetaEntry) => localRepository.getAppMeta(appID, key).map(ensureObject),
+            patchAppMeta: (key: keyof AppMetaEntry, value: any) => localRepository.patchAppMeta(appID, key, ensureObject(value))
         } as AppMetaManager;
 
     } else {
         return {
-            getAppMeta: (key?: keyof AppMetaEntry) => platformRepository.getAppMeta(appID, key),
-            patchAppMeta: (key: keyof AppMetaEntry, value: any) => platformRepository.patchAppMeta(appID, key, value)
+            getAppMeta: (key?: keyof AppMetaEntry) => platformRepository.getAppMeta(appID, key).map(ensureObject),
+            patchAppMeta: (key: keyof AppMetaEntry, value: any) => platformRepository.patchAppMeta(appID, key, ensureObject(value))
         } as AppMetaManager;
     }
 
+}
+
+function ensureObject(value) {
+    if (Object.prototype.isPrototypeOf(value)) {
+        return value;
+    }
+
+    return {};
 }
 
 export const APP_META_MANAGER = new InjectionToken("appMetaManager");
