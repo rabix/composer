49f22407bd8b65fc6fadff88f0e6dc95715c013b
nikolabulatovic <nikola.bulatovic@sbgenomics.com>
2017-12-28 17:22:24 +0100

    fix(ui) - Set appID in PublishModalComponent

--- src/app/editor-common/app-editor-base/app-editor-base.ts
+++ src/app/editor-common/app-editor-base/app-editor-base.ts
@@ -373,13 +373,14 @@ export abstract class AppEditorBase extends DirectiveBase implements StatusContr
         this.syncModelAndCode(true).then(() => {
             const modal = this.modal.fromComponent(PublishModalComponent, "Push an App");
             modal.appContent = this.getModelText(true);
+            modal.appID = this.dataModel.id;
 
             modal.published.take(1).subscribeTracked(this, (appID) => {
 
                 const tab = this.workbox.getOrCreateAppTab({
                     id: AppHelper.getRevisionlessID(appID),
                     type: this.dataModel.class,
-                    label: modal.inputForm.get("name").value,
+                    label: modal.inputForm.get("id").value,
                     isWritable: true,
                     language: "json"
 
